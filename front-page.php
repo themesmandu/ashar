112c0533daa19d2f67697584af70978945eb466f
rupeshchamling <rupeshrakhamicha@gmail.com>
2019-06-21 18:57:11 +0545

    front page section html and css developed

--- front-page.php
+++ front-page.php
@@ -89,7 +89,42 @@ get_header();
 </section>
 
 <section class="section_two">
-    
+    <div class="container">
+        <div class="row">
+            <div class="content-wrap">
+                <div class="content">
+                    <h2 class="section_title">We're motivated by the desire to achieve.</h2>
+                    <div class="column">
+                        <i class="fas fa-globe"></i>
+                        <div class="column-content">
+                            <h4>Successful projects</h4>
+                            <p>Since its establishment in 2000, Brook Creative has been focusing on project management & implementation through cooperation.</p>
+                        </div>
+                    </div>
+
+                    <div class="column">
+                        <i class="far fa-calendar"></i>
+                        <div class="column-content">
+                            <h4>Unique designs</h4>
+                            <p>In total, Brook Creative has created more than 2000 projects related to constructional designing and landscaping worldwide.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <figure>
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/section_two_content_img.png'; ?>" />
+            </figure>
+        </div>
+    </div>
+</section>
+
+<section class="section_three">
+    <div class="container">
+        <div class="content">
+            <h3>We're motivated by the desire to achieve.</h3>
+            <a href="#" class="sec_btn">Find out more</a>
+        </div>
 </section>
 
 <?php
