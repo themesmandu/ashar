e2a455deb38e6ab438320de3d6e6f497dd1cc41d
sauravxtha <sauravxtha@gmail.com>
2019-06-23 11:22:06 +0545

    fixed header

--- header.php
+++ header.php
@@ -36,7 +36,7 @@
 								$ashar_site_title            = get_bloginfo( 'name' );
 								$ashar_site_title_letter     = substr( $ashar_site_title, -2, 1 );
 								$ashar_site_title_first_part = substr( $ashar_site_title, 0, -2 );
-								$ashar_site_title_last_part  = substr( $ashar_site_title, -1 ); 
+								$ashar_site_title_last_part  = substr( $ashar_site_title, -1 );
 								if ( is_front_page() && is_home() ) :
 									?>
 									<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><strong><?php echo esc_html( $ashar_site_title_first_part ); ?></strong><span><?php echo esc_html( $ashar_site_title_letter ); ?></span><strong><?php echo esc_html( $ashar_site_title_last_part ); ?></strong></a></h1>
@@ -79,7 +79,7 @@
 					</div><!-- .navbar-wrap -->
 				</div><!-- .container -->
 			</nav><!-- #site-navigation -->
-
+			<?php if ( is_front_page() ) : ?>
 			<div id="image_slider">
 				<div class="slider_images">
 					<ul>
@@ -149,6 +149,7 @@
 					<a href="#" title="three"><span>03</span></a>
 				</div>
 			</div>
+						<?php endif; ?>
 		</header><!-- #masthead -->
 
 		<div id="content" class="site-content">
