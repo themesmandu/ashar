ce93baeeafe47c68c3a9db2e271f03073145e615
sauravxtha <sauravxtha@gmail.com>
2019-06-27 10:20:17 +0545

    standards

--- inc/comment-callback.php
+++ inc/comment-callback.php
@@ -16,11 +16,9 @@
  *
  * @param string $depth comment depth.
  */
-function ashar_custom_comments($comment, $args, $depth)
-{
-
+function ashar_custom_comments( $comment, $args, $depth ) {
 	// Get correct tag used for the comments.
-	if ('div' === $args['style']) {
+	if ( 'div' === $args['style'] ) {
 		$tag       = 'div';
 		$add_below = 'comment';
 	} else {
@@ -28,28 +26,29 @@ function ashar_custom_comments($comment, $args, $depth)
 		$add_below = 'div-comment';
 	} ?>
 
-	<<?php echo $tag; ?> <?php comment_class(empty($args['has_children']) ? '' : 'parent'); ?> id="comment-<?php comment_ID(); ?>">
+	<<?php echo $tag; ?> <?php comment_class( empty( $args['has_children'] ) ? '' : 'parent' ); ?> id="comment-<?php comment_ID(); ?>">
 
 		<?php
 		// Switch between different comment types.
-		switch ($comment->comment_type): case 'pingback':
+		switch ( $comment->comment_type ) :
+			case 'pingback':
 			case 'trackback':
 				?>
-			<div class="pingback-entry"><span class="pingback-heading"><?php esc_html_e('Pingback:', 'textdomain'); ?></span> <?php comment_author_link(); ?></div>
-			<?php
-			break;
-		default:
-			if ('div' != $args['style']) {
-				?>
+			<div class="pingback-entry"><span class="pingback-heading"><?php esc_html_e( 'Pingback:', 'textdomain' ); ?></span> <?php comment_author_link(); ?></div>
+				<?php
+				break;
+			default:
+				if ( 'div' != $args['style'] ) {
+					?>
 				<div id="div-comment-<?php comment_ID(); ?>" class="comment-meta">
 				<?php } ?>
 				<div class="comment-author vcard">
 					<figure>
 						<?php
 						// Display avatar unless size is set to 0.
-						if ($args['avatar_size'] != 0) {
-							$avatar_size = !empty($args['avatar_size']) ? $args['avatar_size'] : 70; // set default avatar size
-							echo get_avatar($comment, $avatar_size);
+						if ( $args['avatar_size'] != 0 ) {
+							$avatar_size = ! empty( $args['avatar_size'] ) ? $args['avatar_size'] : 70; // set default avatar size
+							echo get_avatar( $comment, $avatar_size );
 						}
 						?>
 					</figure>
@@ -57,29 +56,29 @@ function ashar_custom_comments($comment, $args, $depth)
 					<div class="comment-metadata">
 						<?php
 						// Display author name.
-						printf(__('<span class="fn">%s</span> ', 'textdomain'), get_comment_author_link());
+						printf( __( '<span class="fn">%s</span> ', 'textdomain' ), get_comment_author_link() );
 						?>
 
 						<div class="comment-details">
 							<div class="comment-text"><?php comment_text(); ?></div><!-- .comment-text -->
 							<?php
 							// Display comment moderation text.
-							if ($comment->comment_approved === '0') {
+							if ( $comment->comment_approved === '0' ) {
 								?>
-								<em class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.', 'textdomain'); ?></em><br />
-							<?php
-						}
-						?>
+								<em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'textdomain' ); ?></em><br />
+								<?php
+							}
+							?>
 
 						</div><!-- .comment-details -->
 
 						<div class="date-warp">
 
-							<a href="<?php echo htmlspecialchars(get_comment_link($comment->comment_ID)); ?>">
+							<a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ); ?>">
 								<?php
 								/* translators: 1: date, 2: time */
 								printf(
-									__('%1$s at %2$s', 'textdomain'),
+									__( '%1$s at %2$s', 'textdomain' ),
 									get_comment_date(),
 									get_comment_time()
 								);
@@ -87,33 +86,33 @@ function ashar_custom_comments($comment, $args, $depth)
 							</a>
 
 							<div class="reply">
-								<?php
-								// Display comment reply link.
-								comment_reply_link(
-									array_merge(
-										$args,
-										array(
-											'add_below' => $add_below,
-											'depth'     => $depth,
-											'max_depth' => $args['max_depth'],
+									<?php
+									// Display comment reply link.
+									comment_reply_link(
+										array_merge(
+											$args,
+											array(
+												'add_below' => $add_below,
+												'depth' => $depth,
+												'max_depth' => $args['max_depth'],
+											)
 										)
-									)
-								);
-								?>
+									);
+									?>
 							</div>
 						</div>
-						<?php
-						edit_comment_link(__('(Edit)', 'textdomain'), '  ', '');
-						?>
+							<?php
+							edit_comment_link( __( '(Edit)', 'textdomain' ), '  ', '' );
+							?>
 					</div><!-- .comment-meta -->
 				</div><!-- .comment-author -->
-				<?php
-				if ('div' !== $args['style']) {
-					?>
+					<?php
+					if ( 'div' !== $args['style'] ) {
+						?>
 				</div>
-			<?php
-		}
-		// IMPORTANT: Note that we do NOT close the opening tag, WordPress does this for us.
-		break;
+						<?php
+					}
+					// IMPORTANT: Note that we do NOT close the opening tag, WordPress does this for us.
+				break;
 endswitch; // End comment_type check.
 }
